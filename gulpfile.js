8a52fc18b5e7cb30d8eedf7a367ecb6ada91b781
KuzmiCz85 <lukas.akim@centrum.cz>
2022-05-14 20:44:59 +0200

    Created sass task in gulpfile.js | Update related files

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,34 @@
-// Configuration and tasks for Gulp toolkit
+'use strict';
+
+// Configuration
+const settings = {
+  sass: {
+    source: 'source/sass/style.scss',
+    target: 'test/css/',
+    filename: 'style.css'
+  }
+}
+
+// Gulp plugins
+
+// Gulp
+const gulp = require('gulp');
+  // Merge files
+  const concat = require('gulp-concat');
+// Create css from sass files
+const sass = require('gulp-sass')(require('sass'));
+
+// Tasks
+
+// Sass compile
+function sassCompile() {
+  return gulp.src(settings.sass.source)
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(concat(settings.sass.filename))
+    .pipe(gulp.dest(settings.sass.target));
+}
+
+exports.sassCompile = sassCompile;
+
+// Default task
+exports.default = sassCompile;
