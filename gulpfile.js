fc616b825ca1810b7fa68d2ee0d88c48a30178d0
KuzmiCz85 <lukas.akim@centrum.cz>
2022-05-16 17:31:18 +0200

    Created new tasks in gulpfile.js | Added related files

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,23 @@
 
 // Configuration
 const settings = {
+  css: {
+    source: 'source/sass/style.scss',
+    target: 'test/css/',
+    filename: 'style.css'
+  },
   js: {
     source: 'source/js/*.js',
     target: 'test/js/',
     filename: 'script.js'
   },
-  sass: {
-    source: 'source/sass/style.scss',
-    target: 'test/css/',
-    filename: 'style.css'
+  html: {
+    source: 'source/pages/*.{html,htm,php}',
+    target: 'test/'
+  },
+  img: {
+    source: 'source/images/**/*.{gif,jpg,jpeg,png}',
+    target: 'test/images/'
   }
 }
 
@@ -25,25 +33,35 @@ const sass = require('gulp-sass')(require('sass'));
 
 // Tasks
 
-// Sass compile
-function sassCompile() {
-  return gulp.src(settings.sass.source)
+// Css - compile from sass
+function taskCss() {
+  return gulp.src(settings.css.source)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(concat(settings.sass.filename))
-    .pipe(gulp.dest(settings.sass.target));
+    .pipe(concat(settings.css.filename))
+    .pipe(gulp.dest(settings.css.target));
 }
 
-exports.sassCompile = sassCompile;
-
-// JavaScript merge files
-function taskJsMerge() {
+// JavaScript - merge and minify files
+function taskJs() {
   return gulp
     .src(settings.js.source)
     .pipe(concat(settings.js.filename))
     .pipe(gulp.dest(settings.js.target));
 }
 
-exports.taskJsMerge = taskJsMerge;
+// Html - copy and render pages
+function taskHtml() {
+  return gulp.src(settings.html.source)
+  // add nunjucks render
+  .pipe(gulp.dest(settings.html.target));
+}
+
+// Images - copy and optimize
+function taskImg() {
+  return gulp.src(settings.img.source)
+  // add imagemin
+  .pipe(gulp.dest(settings.img.target));
+}
 
 // Default task
-exports.default = sassCompile;
+exports.default = gulp.parallel(taskCss, taskJs, taskHtml, taskImg);
